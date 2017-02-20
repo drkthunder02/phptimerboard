7c1a8291a25d72ae5992a13cf5e61d1c4d8ea747
drkthunder02 <mancuso.christopher@gmail.com>
2017-02-19 23:39:32 -0600

    initial commit

---
+++ functions/class/sessions.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Custom\Sessions;
+
+class session {
+    //The database object in order to store the session data in a mysql database
+    private $db;
+    
+    public function __construct(){
+        // Set handler to overide SESSION
+        session_set_save_handler(
+            array($this, "_open"),
+            array($this, "_close"),
+            array($this, "_read"),
+            array($this, "_write"),
+            array($this, "_destroy"),
+            array($this, "_gc")
+        );
+        
+        session_start();
+    }
+    
+    public function _open() {
+        $config = parse_ini_file(__DIR__.'/../configuration/database.ini');
+        //Setup our db object
+        $this->db = new \Simplon\Mysql\Mysql(
+            'localhost',
+            'root',
+            'P@55w0rd!',
+            'eveotsv2'
+        );
+        
+        if($this->db) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function _close() {
+        if($this->db->close()) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public function _read($id) {
+        $row = $this->db->fetchRow('SELECT data FROM sessions WHERE id= :id', array('id' => $id));
+        
+        if($row) {
+            return $row['data'];
+        } else {
+            return '';
+        }
+    }
+    
+    public function _write($id, $data){
+        // Create time stamp
+        $access = time();
+        
+        $result = $this->db->replace('sessions', array('id' => $id, 'access' => $access, 'data' => $data));
+        
+        if($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function _destroy($id){
+        $result = $this->db->delete('sessions', array('id' => $id));
+    
+        if($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }    
+    
+    public function _gc($max) {
+        $old = time() - $max;
+        
+        // custom conditions query
+        $condsQuery = 'access < :old';
+        $result = $this->db->delete('sessions', array('old' => $old), $condsQuery);
+        
+        if($result) {
+            return true;
+        } else {
+            return false;
+        }
+       
+    }
+    
+}
---
+++ functions/registry.php
@@ -0,0 +1,6 @@
+<?php
+
+//Autoload Functions
+require_once __DIR__.'/../vendor/autoload.php';
+
+?>
