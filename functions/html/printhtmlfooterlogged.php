086f26d5c3221cd9996254fb45a9f2460e264862
drkthunder02 <mancuso.christopher@gmail.com>
2017-02-23 00:12:31 -0600

    footer and main page

---
+++ functions/html/printhtmlfooterlogged.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function PrintHTMLFooterLogged() {
+    printf("<hr>");
+    printf("PHP Timerboard written by Lowjack Tzetsu.");
+}
+
+?>
--- timer/index.php
+++ timer/index.php
@@ -24,25 +24,47 @@ PrintNavBarLogged($_SESSION['Character'], $_SESSION['AccessLevel']);
 
 $currentTime = time();
 
-//Print out the timers that are going on currently since we have printed out the navigation bar at the top
+//Print out the timers that are going on currently since we have printed out the navigation bar at the to"
 $timers = $db->fetchRowMany('SELECT * FROM Timers WHERE EVETime >= :now', array('now' => $currentTime));
+//Print out the table header and the start of the body tag
+printf("<div class=\"table table-striped\">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Stage</th>
+                    <th>Region</th>
+                    <th>System</th>
+                    <th>Planet</th>
+                    <th>Moon</th>
+                    <th>Owner</th>
+                    <th>EVE Time</th>
+                    <th>Remaining</th>
+                    <th>Notes</th>
+                    <th>User</th>            
+                </tr>
+            </thead><tbody>");
 foreach($timers as $timer) {
+    $now = time();
+    $remaining = $timer['EVETime'] - $now;
+    $remaining = date("Y-m-d H:i:s", $remaining);
+    $eveTime = date("Y-m-d H:i:s", $timer['EVETime']);
     
+    printf("<tr>");
+    printf("<td>" . $timer['Type'] . "</td>");
+    printf("<td>" . $timer['Stage'] . "</td>");
+    printf("<td>" . $timer['Region'] . "</td>");
+    printf("<td>" . $timer['System'] . "</td>");
+    printf("<td>" . $timer['Planet'] . "</td>");
+    printf("<td>" . $timer['Moon'] . "</td>");
+    printf("<td>" . $timer['Owner'] . "</td>");
+    printf("<td>" . $eveTime . "</td>");
+    printf("<td>" . $remaining . "</td>");
+    printf("<td>" . $timer['Notes'] . "</td>");
+    printf("<td>" . $timer['User'] . "</td>");
+    printf("</tr>");
 }
+printf("</tbody></div>");
 
-$_SESSION['logged'] = true;
-$_SESSION['Character'] = $char['name'];
-$_SESSION['CharacterID'] = $charID;
-$_SESSION['AccessLevel'] = $charAccess;
-$_SESSION['CorpAllow'] = $corpAccess;
-$_SESSION['AllyAllow'] = $allyAccess;
-
-printf("We made it here finally.<br>");
-
-
+PrintHTMLFooterLogged();
 
 ?>
-
-<div class="table table-striped">
-    
-</div>
