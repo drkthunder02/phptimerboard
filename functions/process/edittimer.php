be9ac7b4e5c3bc0cc1867b66dcd044508fd6f23b
drkthunder02 <mancuso.christopher@gmail.com>
2017-04-02 23:13:38 -0500

    finished edittimer.php

--- functions/process/edittimer.php
+++ functions/process/edittimer.php
@@ -27,55 +27,79 @@ if($_SESSION['logged'] != true) {
 //Get the parameters of the timer to be edited
 if(isset($_POST['id'])) {
     $timerId = filter_input(INPUT_POST, "id", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
+    //Get the values of the old timer from the id
+    $oldTimer = $db->fetchRow('SELECT * FROM Timers WHERE id= :old', array('old' => $timerId));
+    
+    //Get all of the other POST values
+    if(isset($_POST['Type'])) {
+        $type = filter_input(INPUT_POST, "Type", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['Type'] != $type) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Type' => $type));
+        }
+    }
+
+    if(isset($_POST['Stage'])) {
+        $stage = filter_input(INPUT_POST, "Stage", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['Stage'] != $stage) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Stage' => $stage));
+        }
+    }
+
+    if(isset($_POST['Region'])) {
+        $region = filter_input(INPUT_POST, "Region", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['Region'] != $region) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Region' => $region));
+        }
+    } 
+
+    if(isset($_POST['System'])) {
+        $system = filter_input(INPUT_POST, "System", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['System'] != $system) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('System' => $system));
+        }
+    }
+
+    if(isset($_POST['Planet'])) {
+        $planet = filter_input(INPUT_POST, "Planet", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['Planet'] != $planet) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Planet' => $planet));
+        }
+    } 
+
+    if(isset($_POST['Moon'])) {
+        $moon = filter_input(INPUT_POST, "Moon", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($oldTimer['Moon'] != $moon) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Moon' => $moon));
+        }
+    } 
+
+    if(isset($_POST['Time'])) {
+        $time = filter_input(INPUT_POST, "Time", FILTER_SANITIZE_SPECIAL_CHARS);
+        $time = strtotime($time);
+        if($oldTimer['Time'] != $time) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Time' => $time));
+        }
+    } 
+
+    if(isset($_POST['Notes'])) {
+        $notes = filter_input(INPUT_POST, "Notes", FILTER_SANITIZE_SPECIAL_CHARS);
+        if($oldTimer['Notes'] != $notes) {
+            $db->update('Timers', array('id' => $oldTimer['id']), array('Notes' => $notes));
+        }
+    }
+    
+    //After all modifications are completed, set the header info
+    $location = 'http://' . $_SERVER['HTTP_HOST'];
+    $location = $location . dirname($_SERVER['PHP_SELF']) . '/../../timer/index.php';
+    
 } else {
     $timerId = NULL;
+    $location = 'http://' . $_SERVER['HTTP_HOST'];
+    $location = $location . dirname($_SERVER['PHP_SELF']) . '/../../timer/index.php';
 }
 
-if(isset($_POST['Type'])) {
-    $type = filter_input(INPUT_POST, "Type", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $type = NULL;
-}
+//Go to the main site for timers in the timerboard
+header("Location: $location");
 
-if(isset($_POST['Stage'])) {
-    $stage = filter_input(INPUT_POST, "Stage", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $stage = NULL;
-}
-
-if(isset($_POST['Region'])) {
-    $region = filter_input(INPUT_POST, "Region", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $region = NULL;
-}
-
-if(isset($_POST['System'])) {
-    $system = filter_input(INPUT_POST, "System", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $system = NULL;
-}
-
-if(isset($_POST['Planet'])) {
-    $planet = filter_input(INPUT_POST, "Planet", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $planet = NULL;
-}
-
-if(isset($_POST['Moon'])) {
-    $moon = filter_input(INPUT_POST, "Moon", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-} else {
-    $moon = NULL;
-}
-
-if(isset($_POST['Time'])) {
-    $time = filter_input(INPUT_POST, "Time", FILTER_SANITIZE_SPECIAL_CHARS);
-} else {
-    $time = NULL;
-}
-
-if(isset($_POST['Notes'])) {
-    $notes = filter_input(INPUT_POST, "Notes", FILTER_SANITIZE_SPECIAL_CHARS);
-} else {
-    $notes = NULL;
-}
-
+?>
