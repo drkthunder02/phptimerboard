1c192e147d925af61569ec04eace312bf2b67209
drkthunder02 <mancuso.christopher@gmail.com>
2020-05-23 04:37:32 -0500

    added framework for controller

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function displayAddAlliance() {
+
+    }
+
+    public function displayAddCorporation() {
+
+    }
+
+    public function displayAddCharacter() {
+
+    }
+
+    public function displayRemoveAlliance() {
+
+    }
+
+    public function displayRemoveCorporation() {
+
+    }
+
+    public function displayRemoveCharacter() {
+        
+    }
+}
---
+++ app/Http/Controllers/Timers/TimerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Timers;
+
+use Illuminate\Http\Request;
+
+class TimerController extends Controller
+{
+    public function __construct() {
+        
+    }
+
+    public function displayAddTimer() {
+
+    }
+
+    public function displayRemoveTimer() {
+
+    }
+
+    public function displayModifyTimer() {
+
+    }
+
+    public function displayTimers() {
+        
+    }
+}
